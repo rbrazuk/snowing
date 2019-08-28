f2abbcc93c7a7bdd3d691ec0dc462a9bcc83d97f
Ross Brazuk <ross.brazuk@gmail.com>
2019-08-28 09:10:00 -0400

    initial commit plus project setup

---
+++ src/components/IconsBar.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class IconsBar extends Component {
+    render() {
+        return <div>Icons Bar</div>
+    }
+}
+
+export default IconsBar;
---
+++ src/components/MenuIcon.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class MenuIcon extends Component {
+    render() {
+        return <div>menu icon</div>
+    }
+}
+
+export default MenuIcon;
