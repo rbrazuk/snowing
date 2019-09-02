0cb419c7b532163002ed788ec6a284d9eb7c0c79
Ross Brazuk <ross.brazuk@gmail.com>
2019-09-02 09:01:02 -0400

    Refactored MenuOverlay into functional component

--- src/components/MenuOverlay.js
+++ src/components/MenuOverlay.js
@@ -1,31 +1,17 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import './MenuOverlay.css';
 
-class MenuOverlay extends Component {
-    constructor(props) {
-        super(props);
-        console.log(props);
-    }
-    render() {
-        return (
-            <div className='menu-overlay'>
-                <ul className='menu'>
-                    <li>news</li>
-                    <li>shows</li>
-                    <li>music</li>
-                    <li>merch</li>
-                    <li>contact</li>
-                </ul>
-            </div>
-        )      
-    }
+export default (props) => {
+    return (
+        <div className='menu-overlay'>
+            <ul className='menu'>
+                <li>news</li>
+                <li>shows</li>
+                <li>music</li>
+                <li>merch</li>
+                <li>contact</li>
+            </ul>
+        </div>
+    );      
 }
 
-function mapStateToProps(state) {
-    return {showMenu: state.showMenu};
-}
-
-
-
-export default connect(mapStateToProps)(MenuOverlay);
