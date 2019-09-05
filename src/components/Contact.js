6d1927ab6cb5fd163624053ffc1dd3aa38a0eed3
Ross Brazuk <ross.brazuk@gmail.com>
2019-09-05 14:17:13 -0400

    Fleshed out the Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,8 +4,18 @@ import './Contact.css';
 class Contact extends Component {
     render() {
         return (
-            <div>
-                <div>Contact</div>
+            <div className='contact'>
+                <p>
+                    You can email us at <a href='mailto:snowingtheband@gmail.com'>snowingtheband@gmail.com</a>
+                </p>
+                <p>
+                    <a href='http://www.squareofopposition.com/'>Square of Opposition</a> and <a href='http://countyourluckystars.limitedrun.com/'>Count Your Lucky Stars</a> released 
+                    our records. Please support them!
+                </p>
+                <p>
+                    You can click on the icons below to get to our various social media 
+                    accounts, but I bet you figured that out on your own.
+                </p>
             </div>)
     }
 }
